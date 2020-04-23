b48389fe0ef050a053fcc885a4c3aadc984a175d
Manuel <manueldev1810@gmail.com>
2020-04-22 22:41:13 -0400

    SingleChildScrollView on body and transaction

--- 4.Style/expenses_app/lib/main.dart
+++ 4.Style/expenses_app/lib/main.dart
@@ -20,27 +20,31 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          //The card by default take the size of its child, so we need to change the size of the text
-          //But Text take the size of its parent
-          //So put the text in a container to put a width
+      //SingleChildScrollView has to be in the body to know what size to take, the height to take.
+      //If you have to put it in another part, you can put a container with a height and then put it
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            //The card by default take the size of its child, so we need to change the size of the text
+            //But Text take the size of its parent
+            //So put the text in a container to put a width
 
-          //Another way to it is put the card in a container with its wild, so the thing here
-          //The thing is that card depends on the size of its child, unless
-          //Unless the card itself it's grap in a contianer
-          Container(
-            width: double.infinity,
-            child: Card(
-              //Now the size of the card depends of the size of the container that we can control
-              color: Colors.blue,
-              child: Text('CHART!'),
-              elevation: 5,
+            //Another way to it is put the card in a container with its wild, so the thing here
+            //The thing is that card depends on the size of its child, unless
+            //Unless the card itself it's grap in a contianer
+            Container(
+              width: double.infinity,
+              child: Card(
+                //Now the size of the card depends of the size of the container that we can control
+                color: Colors.blue,
+                child: Text('CHART!'),
+                elevation: 5,
+              ),
             ),
-          ),
-          UserTransactions(),
-        ],
+            UserTransactions(),
+          ],
+        ),
       ),
     );
   }

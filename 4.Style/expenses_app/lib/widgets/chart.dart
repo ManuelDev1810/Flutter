d8f42ca0dfc3bc83db2d6df636004b21ef463f85
Manuel <manueldev1810@gmail.com>
2020-04-25 14:07:49 -0400

    Finishing the chart bars

--- 4.Style/expenses_app/lib/widgets/chart.dart
+++ 4.Style/expenses_app/lib/widgets/chart.dart
@@ -1,4 +1,5 @@
 import 'package:expenses_app/models/transaction.dart';
+import 'package:expenses_app/widgets/chart_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -11,18 +12,28 @@ class Chart extends StatelessWidget {
     return List.generate(7, (index) {
       //Which day are we generatings, we are subtraing that, if we pass 0 today, 1 yesterday
       final weekDay = DateTime.now().subtract(Duration(days: index));
-      
+
       var totalSum = 0.0;
       for (var i = 0; i < recentTransactions.length; i++) {
         if (recentTransactions[i].date.day == weekDay.day &&
             recentTransactions[i].date.month == weekDay.month &&
             recentTransactions[i].date.year == weekDay.year) {
-              totalSum += recentTransactions[i].amount;
-            }
+          totalSum += recentTransactions[i].amount;
+        }
       }
 
-      //Creatint the map
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      //Creating the map
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    });
+  }
+
+  double get totalSpending {
+    //Fold allow us to change list to another type with a certain logic
+    return gropedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
     });
   }
 
@@ -31,8 +42,24 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: <Widget>[],
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: gropedTransactionValues.map((data) {
+            return Flexible(
+              //The chill is force to use the available space, so it cant grow
+              fit: FlexFit.tight,
+              child: ChartBar(
+                data['day'],
+                data['amount'],
+                totalSpending == 0.0
+                    ? 0.0
+                    : (data['amount'] as double) / totalSpending,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }

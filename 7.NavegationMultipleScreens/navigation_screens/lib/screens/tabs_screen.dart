aa3d5d737f9e46295fcd975c8a439d677eca6513
Manuel <manueldev1810@gmail.com>
2020-05-03 09:08:09 -0400

    Adding a bottom TabBar

--- 7.NavegationMultipleScreens/navigation_screens/lib/screens/tabs_screen.dart
+++ 7.NavegationMultipleScreens/navigation_screens/lib/screens/tabs_screen.dart
@@ -8,38 +8,52 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreen extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritiesScreen(),
+      'title': 'Your Favorities',
+    },
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selecPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          //The TabBar and the DefaultTabController connect to each other by flutter
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(
-                  Icons.category,
-                ),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.star,
-                ),
-                text: 'Favorities',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selecPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        //To use this, that give you a cool effect when changing icons, you have to style your items
+        // type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            // backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
           ),
-        ),
-        //The TabBarView and the DefaultTabController connect to each other by flutter
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritiesScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            // backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.favorite),
+            title: Text('Favorities'),
+          ),
+        ],
       ),
     );
   }

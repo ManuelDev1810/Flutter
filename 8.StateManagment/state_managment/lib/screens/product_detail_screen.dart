726224c8247538496048d35d14fa247e9f89657f
Manuel <manueldev1810@gmail.com>
2020-05-09 14:47:22 -0400

    Adding the product detail

--- 8.StateManagment/state_managment/lib/screens/product_detail_screen.dart
+++ 8.StateManagment/state_managment/lib/screens/product_detail_screen.dart
@@ -10,7 +10,8 @@ class ProductDefailScreen extends StatelessWidget {
     final productId = ModalRoute.of(context).settings.arguments as String;
     //listen:false .. how this method does not need to be rebuild when something changes, we put it false
     //So wont rebuild when something change, this is something you do when you only want data
-    final loadedProduct = Provider.of<Products>(context, listen: false).findById(productId);
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
 
     return Scaffold(
       appBar: AppBar(
@@ -18,7 +19,39 @@ class ProductDefailScreen extends StatelessWidget {
           loadedProduct.title,
         ),
       ),
+      //This is only on case where the phones are not big
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
+      ),
     );
-
   }
 }

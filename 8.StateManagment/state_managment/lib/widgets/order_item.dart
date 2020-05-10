dd8be929709be21f75af1b27ff7ba4b2b986836f
Manuel <manueldev1810@gmail.com>
2020-05-10 10:36:41 -0400

    Making prders expandable

--- 8.StateManagment/state_managment/lib/widgets/order_item.dart
+++ 8.StateManagment/state_managment/lib/widgets/order_item.dart
@@ -1,10 +1,18 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../providers/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
   OrderItem(this.order);
+
+  @override
+  _OrderItem createState() => _OrderItem();
+}
+
+class _OrderItem extends State<OrderItem> {
+  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,17 +21,56 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('\$${order.amount}'),
+            title: Text('\$${widget.order.amount}'),
             subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
             ),
             trailing: IconButton(
               icon: Icon(
-                Icons.expand_more,
+                _expanded ? Icons.expand_less : Icons.expand_more,
               ),
-              onPressed: (){},
+              //This should be a statefull class, our card, orders and products does not care about expanded this
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
           ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              //Min will take eithe r of these two values, it will calculate the first one and..
+              //..if its smaller that the 180 will take it and if its very large will take the 180
+              //So the child has to be a ListView cause if we use 180 and there are more products..
+              //.. well need to scrill down
+              height: min((widget.order.products.length * 20.0 + 10.0), 100),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (prod) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            prod.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '${prod.quantity}x \$${prod.price}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ) 
+                    .toList(),
+              ),
+            ),
         ],
       ),
     );

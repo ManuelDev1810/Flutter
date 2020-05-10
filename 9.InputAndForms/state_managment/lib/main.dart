4ef97a825eb05b3c1f369088c0312d508337aa48
Manuel <manueldev1810@gmail.com>
2020-05-10 19:48:44 -0400

    SnackBars and Undoing

---
+++ 9.InputAndForms/state_managment/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './screens/product_detail_screen.dart';
+import './screens/products_overview_screen.dart';
+import './screens/cart_screen.dart';
+import './screens/orders_screen.dart';
+import './providers/orders.dart';
+import './providers/products.dart';
+import './providers/cart.dart' as Cart;
+
+//DONT USE PROVIDERS ON LOCAL STATE WIDGETS(STATE THAT ONLY WORK IN A WIDGET, LIKE SWITCHIN A BUTTON)
+//DONT USE IT BECAUSE YOU KNOW WHEN YOU ISE IT WILL BE GLOBALLY INSTEAD USE STATEFULL WIDGETS
+
+//I need to provide a class which you then wanna use from different widgets at the height..
+//..possible point of all these widgets which will be interested
+//This means that if ProductsOverviewScreen and ProductDefailScreen wants the data..
+//I have to put the provider on a class above them, which is exactly this class..
+//That does not mean that the build method will run again on this class, only the ones which are listenen
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //Regesring this class with a provider so the child widgets that are LISTEBER will be rebuild..
+    //..when the data changes
+
+    //Here is the better approach for not using value, WHY? Because you will use that instance..
+    //..for al the project, this is for efficenty, whenever you reuse an existing object use value
+
+    //If you replace the page using provider, dont wonrry ChangeNotifierProvider cleans it up for you
+
+    return MultiProvider(
+      //Can listen by both providers everywhere
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart.Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Orders(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDefailScreen.routeName: (_) => ProductDefailScreen(),
+          CardScreen.routeName: (_) => CardScreen(),
+          OrdersScreen.routeName: (_) => OrdersScreen(),
+        },
+      ),
+    );
+  }
+}
---
+++ 9.InputAndForms/state_managment/lib/screens/products_overview_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../widgets/app_drarwer.dart';
+import '../widgets/products_grid.dart';
+import '../widgets/badge.dart';
+import '../providers/cart.dart' as Cart;
+import '../screens/cart_screen.dart';
+
+enum FilterOptions {
+  Favorities,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  bool _showOnlyFavorities = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'MyShop',
+        ),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorities) {
+                  _showOnlyFavorities = true;
+                } else {
+                  _showOnlyFavorities = false;
+                }
+              });
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FilterOptions.Favorities,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+          //It goes up and find the provider in the main class
+          Consumer<Cart.Cart>(
+            builder: (_, card, ch) => Badge(
+              child: ch,
+              value: card.itemCount.toString(),
+            ),
+            //This dont need to be rebuild
+            child: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+              ),
+              onPressed: () {
+                Navigator.of(context).pushNamed(CardScreen.routeName);
+              },
+            ),
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      //I am taking the grid away cause i dont wanna rebuild the appBar of this widget
+      //Im not using _showOnlyFavorities with the provider cause it will change the list everywhere
+      //I am using locally so i transform this widget to statefull
+      body: ProductsGrid(_showOnlyFavorities),
+    );
+  }
+}

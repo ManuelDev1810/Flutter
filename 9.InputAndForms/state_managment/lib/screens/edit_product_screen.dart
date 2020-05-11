589173f03eb44595fceb194bbfcbd73bf5be7594
Manuel <manueldev1810@gmail.com>
2020-05-11 07:00:53 -0400

    Input Focus

--- 9.InputAndForms/state_managment/lib/screens/edit_product_screen.dart
+++ 9.InputAndForms/state_managment/lib/screens/edit_product_screen.dart
@@ -7,6 +7,8 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreen extends State<EditProductScreen> {
+  //FocusNode save the focusNode of an input
+  final _priceFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +25,15 @@ class _EditProductScreen extends State<EditProductScreen> {
                 //This controls what the bottom right button in the soft keyboard will show..
                 //..whether is a checkmark or a done text or next input
                 textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+                TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
               ),
             ],
           ),

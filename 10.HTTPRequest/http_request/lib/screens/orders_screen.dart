c3dc0f14d693d1e9549209e6d6223a21ea10f89b
Manuel <manueldev1810@gmail.com>
2020-05-25 09:14:14 -0400

    Using FutureBuilder

--- 10.HTTPRequest/http_request/lib/screens/orders_screen.dart
+++ 10.HTTPRequest/http_request/lib/screens/orders_screen.dart
@@ -4,44 +4,57 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/app_drarwer.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
+  // var _isLoading = false;
 
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  //Here ill use initState with the trick we mention in products
-  @override
-  void initState() {
-    // Future.delayed(Duration.zero).then((_) {});
-    //This will run before build runs
-    _isLoading = true;
-    Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
+  // //Here ill use initState with the trick we mention in products
+  // @override
+  // void initState() {
+  //   // Future.delayed(Duration.zero).then((_) {});
+  //   //This will run before build runs
+  //   // _isLoading = true;
+  //   // Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
+  //   //   setState(() {
+  //   //     _isLoading = false;
+  //   //   });
+  //   // });
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    print('building orders');
+    //Gotta get this put cause we will get in a infinitive loop, because when this is done
+    //Because when gettingAndSetting the orders is done it will call notifyListener so build..
+    //..method will run again and gettingAndSetting will run again into an infinitive loop
+    //Solucion is put orderData in a consumer when we need to use it and thats it
+    // final orderData = Provider.of<Orders>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Ordes'),
-      ),
-      drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-            ),
-    );
+        appBar: AppBar(
+          title: Text('Your Ordes'),
+        ),
+        drawer: AppDrawer(),
+        //FutureBuilder, start listening to the future variable so add the then and catch methods for ya
+        //
+        body: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (dataSnapshot.error != null) {
+              return Center(child: Text('An Error occurred'));
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+                ),
+              );
+            }
+          },
+        ));
   }
 }

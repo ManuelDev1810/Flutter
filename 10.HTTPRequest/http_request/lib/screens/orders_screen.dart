9048d2f442dd89a5957e70ce7a2992d778ed0f45
Manuel <manueldev1810@gmail.com>
2020-05-25 08:37:53 -0400

    Fetch and Set Orders

--- 10.HTTPRequest/http_request/lib/screens/orders_screen.dart
+++ 10.HTTPRequest/http_request/lib/screens/orders_screen.dart
@@ -4,8 +4,31 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/app_drarwer.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
+
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  var _isLoading = false;
+
+  //Here ill use initState with the trick we mention in products
+  @override
+  void initState() {
+    // Future.delayed(Duration.zero).then((_) {});
+    //Remeber this runs after the build
+    setState(() {
+      _isLoading = true;
+    });
+    Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +38,12 @@ class OrdersScreen extends StatelessWidget {
         title: Text('Your Ordes'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: orderData.orders.length,
+              itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+            ),
     );
   }
 }
